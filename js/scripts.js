ee2b886b48f7abeec74f6de60296a632ec7a672a
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-20 12:57:42 -0800

    Move forEach loop into returnNumbers function, which put all logic into back-end section

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 //*****BACK-END*****
 
-var numbersArray = [];
-
 var numCheck = function(userInput) { // Checks whether the user enters a number, and if so, whether it's above 0
   if (!userInput || userInput <= 0) {
     alert("Please enter a valid number in the field.");
@@ -9,10 +7,25 @@ var numCheck = function(userInput) { // Checks whether the user enters a number,
 };
 
 var returnNumbers = function(userInput) { // Adds each number up to and including what the user entered into an array, and shows output section
+  var numbersArray = [];
+
   for (var i = 0; i < userInput; i++) {
     numbersArray.push([i + 1]);
     $("#answer_section").show();
   }
+
+  numbersArray.forEach(function(number) { // Runs through numbersArray to determine which return special strings
+    if (number % 15 === 0) { // If number is divisible by fifteen, return "ping-pong"
+      number = "ping-pong"
+    } else if (number % 3 === 0) { // If number is divisible by three, return "ping"
+      number = "ping"
+    } else if (number % 5 === 0) { // If number is divisible by five, return "pong"
+      number = "pong"
+    }
+
+    $("ul").append("<li>" + number + "</li>");
+    $("#answer").text(userInput);
+  });
 };
 
 //*****FRONT-END*****
@@ -25,19 +38,5 @@ $(function() {
 
     numCheck(userInput);
     returnNumbers(userInput);
-
-    numbersArray.forEach(function(number) { // Runs through numbersArray to determine which return special strings
-      if (number % 15 === 0) { // If number is divisible by fifteen, return "ping-pong"
-        number = "ping-pong"
-      } else if (number % 3 === 0) { // If number is divisible by three, return "ping"
-        number = "ping"
-      } else if (number % 5 === 0) { // If number is divisible by five, return "pong"
-        number = "pong"
-      }
-
-      $("ul").append("<li>" + number + "</li>");
-    });
-
-    $("#answer").text(userInput);
   });
 });
