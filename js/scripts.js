2dfdcbfd040468cc6a4a56f4cdf008792f71dbf5
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-19 23:04:34 -0800

    Add ability to return all number values up to the number the user entered, add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 //*****BACK-END*****//
 
+var numbersArray = [];
+
 var numCheck = function(userInput) {
   if (!userInput || userInput <= 0) {
     alert("Please enter a valid number in the field.");
+  }
+};
+
+var returnNumbers = function(userInput) {
+  for (var i = 0; i < userInput; i++) {
+    numbersArray.push([i + 1]);
   }
 };
 
@@ -11,9 +19,18 @@ var numCheck = function(userInput) {
 $(function() {
   $("#number_input").submit(function(event) {
     event.preventDefault();
+    $("ul").empty();
 
     var userInput = parseInt($("#user_input").val());
 
     numCheck(userInput);
+    returnNumbers(userInput);
+
+    $("#answer_section").show();
+    $("ul").append("<li>" + numbersArray + "</li>");
+  });
+
+  $("#reset_button").click(function() {
+    $("#answer_section").hide();
   });
 });
